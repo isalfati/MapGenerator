3d576e12dceef2d3ef9a8a41a369829a5e1489ad
isalfati <ivansalfatift@gmail.com>
2018-10-23 16:32:37 +0200

    Generating noise

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "bitmap_image.hpp"
 #include <noise/noise.h>
 #include "noiseutils.cpp"
 #include <ctime>
@@ -26,7 +25,8 @@ double dRand(double lower, double upper){
 int main()
 {
 	srand(time(0));
-	double seed = dRand(0.0, 1.0);
+	//double seed = dRand(0.0, 1.0);
+	int seed = rand() % INT32_MAX;
 	cout << "dRand(0.0, 1.0): " << seed << endl;
 	
 	cout << "Please, introduce the size of the image: " << endl;
@@ -37,6 +37,10 @@ int main()
 	cin >> xLow >> xHigh >> yLow >> yHigh;
 	
 	module::Perlin myPerlin;
+	
+	//A new Map each time:
+	myPerlin.SetSeed(seed);
+	
 	utils::NoiseMap heightMap;
 	utils::NoiseMapBuilderPlane heightMapBuilder;
 	heightMapBuilder.SetSourceModule (myPerlin);
@@ -49,6 +53,18 @@ int main()
 	utils::Image image;
 	renderer.SetSourceNoiseMap(heightMap);
 	renderer.SetDestImage(image);
+	
+	// Create your own gradient: http://www.colorzilla.com/gradient-editor/
+	renderer.ClearGradient ();
+	renderer.AddGradientPoint (-1.0000, utils::Color (  0,   0, 128, 255)); // deeps
+	renderer.AddGradientPoint (-0.2500, utils::Color (  0,   0, 255, 255)); // shallow
+	renderer.AddGradientPoint (-0.0625, utils::Color (  0, 128, 255, 255)); // shore
+	renderer.AddGradientPoint ( 0.0000, utils::Color (240, 240,  64, 255)); // sand
+	renderer.AddGradientPoint ( 0.1250, utils::Color ( 32, 160,   0, 255)); // grass
+	renderer.AddGradientPoint ( 0.3750, utils::Color (224, 224,   0, 255)); // dirt
+	renderer.AddGradientPoint ( 0.7500, utils::Color (128, 128, 128, 255)); // rock
+	renderer.AddGradientPoint ( 1.0000, utils::Color (255, 255, 255, 255)); // snow	
+	
 	renderer.Render();
 	
 	utils::WriterBMP writer;
@@ -57,6 +73,6 @@ int main()
 	auto str = std::to_string(seed);
 	writer.SetDestFilename(str + ".bmp");
 	writer.WriteDestFile();
-	
+
     cout << "Check the directory. It contains the generated noise image." << endl; 
 }
