20b804ba9a23cc52a59e67d47ff713b5b0a8b4ad
isalfati <ivansalfatift@gmail.com>
2018-10-29 13:36:33 +0100

    Playing

--- mapexample.cpp
+++ mapexample.cpp
@@ -1,5 +1,3 @@
-// billow and rigged
-
 #include <iostream>
 #include <noise/noise.h>
 #include "noiseutils.cpp"
@@ -11,6 +9,9 @@ using namespace std;
 using namespace noise;
 using namespace noise::utils;
 
+#define SEA_LEVEL -0.0
+
+
 string tswp(double value, int n = 3)
 {
 	ostringstream out;
@@ -18,6 +19,122 @@ string tswp(double value, int n = 3)
 	out << fixed << value;
 	return out.str();
 }
+
+int main()
+{
+	srand(time(0));
+	int seed = rand() % INT32_MAX;
+	
+	cout << "This is the seed for this generated map: " << seed << endl;
+	
+	int width, height;
+	cin >> width >> height;
+	double xLow, xHigh, yLow, yHigh;
+	cin >> xLow >> xHigh >> yLow >> yHigh;
+	
+	module::Cylinders water;
+	water.SetFrequency(2);
+	
+	module::Perlin waterGrainNoise;
+	waterGrainNoise.SetSeed(seed);
+	waterGrainNoise.SetFrequency(48.0);
+	waterGrainNoise.SetPersistence(0.5);
+	waterGrainNoise.SetLacunarity(2.735858);
+	waterGrainNoise.SetOctaveCount(3);
+	waterGrainNoise.SetNoiseQuality(QUALITY_STD);
+	
+	module::ScalePoint scaledWaterGrain;
+	scaledWaterGrain.SetSourceModule(0, waterGrainNoise);
+	scaledWaterGrain.SetYScale(0.25);
+	
+	module::ScaleBias waterGrain;
+	waterGrain.SetSourceModule(0, scaledWaterGrain);
+	waterGrain.SetScale(0.25);
+	waterGrain.SetBias(0.125);
+	
+	module::Add combinedWater;
+	combinedWater.SetSourceModule(0, water);
+	combinedWater.SetSourceModule(1, waterGrain);
+	
+	module::Turbulence perturbedWater;
+	perturbedWater.SetSourceModule(0, combinedWater);
+	perturbedWater.SetSeed(seed);
+	perturbedWater.SetFrequency(4.0);
+	perturbedWater.SetPower(1.0/256.0);
+	perturbedWater.SetRoughness(4);
+	
+	module::TranslatePoint translatedWater;
+	translatedWater.SetSourceModule(0, perturbedWater);
+	translatedWater.SetXTranslation(2.0);
+	
+	
+	module::Billow baseFlatTerrain;
+	baseFlatTerrain.SetSeed(seed);
+	baseFlatTerrain.SetFrequency(3.0);
+	
+	module::ScaleBias flatTerrain;
+	flatTerrain.SetSourceModule(0, baseFlatTerrain);
+	flatTerrain.SetScale(0.125);
+	flatTerrain.SetBias(-0.5);
+	
+	// Type of Terrain
+	module::Perlin terrainType;
+	terrainType.SetSeed(seed);	
+	terrainType.SetFrequency(0.5);
+	terrainType.SetPersistence(0.25);
+	
+	// Selector of Terrain based on Perlin terrainType
+	module::Select terrainSelector;
+	terrainSelector.SetSourceModule(0, flatTerrain);
+	//terrainSelector.SetSourceModule(1, water);
+	terrainSelector.SetSourceModule(1, translatedWater);
+	terrainSelector.SetControlModule(terrainType);
+	terrainSelector.SetBounds(0.0, 1000.0);
+	terrainSelector.SetEdgeFalloff(0.125);
+	
+	module::Turbulence finalTerrain;
+	finalTerrain.SetSourceModule(0, terrainSelector);
+	finalTerrain.SetFrequency(4.0);
+	finalTerrain.SetPower(0.125);
+	
+	utils::NoiseMap heightMap;
+	utils::NoiseMapBuilderPlane heightMapBuilder;
+	heightMapBuilder.SetSourceModule(perturbedWater);
+	//heightMapBuilder.SetSourceModule(baseFlatTerrain);
+	//heightMapBuilder.SetSourceModule(flatTerrain);
+	//heightMapBuilder.SetSourceModule(terrainSelector);
+	heightMapBuilder.SetDestNoiseMap(heightMap);
+	heightMapBuilder.SetDestSize(width, height);
+	heightMapBuilder.SetBounds(xLow, xHigh, yLow, yHigh);
+	heightMapBuilder.Build();
+
+	utils::RendererImage renderer;
+	utils::Image image;
+	renderer.SetSourceNoiseMap (heightMap);
+	renderer.SetDestImage (image);
+	renderer.ClearGradient ();
+	
+	//Bluish gradient
+	renderer.AddGradientPoint (-1.0000, utils::Color( 30,  87, 153, 255));
+	//renderer.AddGradientPoint (-0.6000, utils::Color( 41, 137, 216, 255));
+	renderer.AddGradientPoint ( 1.000, utils::Color(125, 185, 232, 255));
+	//renderer.AddGradientPoint ( 1.0000, utils::Color(255, 255, 255, 255));
+	
+	renderer.EnableLight ();
+	renderer.SetLightContrast (3.0);
+	renderer.SetLightBrightness (2.0);	
+	renderer.Render ();	
+	
+	utils::WriterBMP Writer;
+	Writer.SetSourceImage(image);
+	
+	Writer.SetDestFilename(tswp(seed) + ".bmp");
+	Writer.WriteDestFile();
+	
+	cout << "Check the directory. It contains the generated map." << endl;
+}
+
+/*
 
 int main()
 {
@@ -43,19 +160,16 @@ int main()
 	myModule.SetSeed(seed);
 	myModule.SetOctaveCount(10);
 
-	utils::NoiseMap heightMap;
+	utils::NoiseMap heightMap;	
 	utils::NoiseMapBuilderPlane heightMapBuilder;
 	heightMapBuilder.SetSourceModule(myModule);
 	heightMapBuilder.SetDestNoiseMap(heightMap);
 	heightMapBuilder.SetDestSize(width, height); 
 	heightMapBuilder.SetBounds(xLow, xHigh, yLow, yHigh);
+	heightMapBuilder.EnableSeamless(false);
 	heightMapBuilder.Build();
-
-
-
-//Idea: Generar un map y utilizar un circular para intentar generar una isla
-
-
+	
+	//Idea: Generar un map y utilizar un circular para intentar generar una isla
 
 	// Común a todos los mapas.
 	utils::RendererImage renderer;
@@ -63,16 +177,16 @@ int main()
 	renderer.SetSourceNoiseMap (heightMap);
 	renderer.SetDestImage (image);
 	renderer.ClearGradient ();
-	renderer.AddGradientPoint (-1.0000, utils::Color (  0,   0, 128, 255)); // deeps
-	renderer.AddGradientPoint (-0.2500, utils::Color (  0,   0, 255, 255)); // shallow
-	renderer.AddGradientPoint ( 0.0000, utils::Color (  0, 128, 255, 255)); // shore
-	renderer.AddGradientPoint ( 0.0625, utils::Color (240, 240,  64, 255)); // sand
-	renderer.AddGradientPoint ( 0.1250, utils::Color ( 32, 160,   0, 255)); // grass
-	renderer.AddGradientPoint ( 0.3750, utils::Color (224, 224,   0, 255)); // dirt
-	renderer.AddGradientPoint ( 0.7500, utils::Color (128, 128, 128, 255)); // rock
-	renderer.AddGradientPoint ( 1.0000, utils::Color (255, 255, 255, 255)); // snow
-	//renderer.AddGradientPoint ( 0.8700, utils::Color (96, 96, 96, 255));
-	//renderer.AddGradientPoint ( 1.0000, utils::Color (255, 0, 0, 255));
+	renderer.AddGradientPoint (-1.0000 + SEA_LEVEL, utils::Color (  0,   0, 128, 255)); // deeps
+	renderer.AddGradientPoint (-0.2500 + SEA_LEVEL, utils::Color (  0,   0, 255, 255)); // shallow
+	renderer.AddGradientPoint ( 0.0000 + SEA_LEVEL, utils::Color (  0, 128, 255, 255)); // shore
+	renderer.AddGradientPoint ( 0.0625 + SEA_LEVEL, utils::Color (240, 240,  64, 255)); // sand
+	renderer.AddGradientPoint ( 0.1250 + SEA_LEVEL, utils::Color ( 32, 160,   0, 255)); // grass
+	renderer.AddGradientPoint ( 0.8750, utils::Color (224, 224,   0, 255)); // dirt
+	renderer.AddGradientPoint ( 1.0000, utils::Color (128, 128, 128, 255)); // rock
+	//renderer.AddGradientPoint ( 0.3750, utils::Color (224, 224,   0, 255)); // dirt
+	//renderer.AddGradientPoint ( 0.8000, utils::Color (128, 128, 128, 255)); // rock
+	//renderer.AddGradientPoint ( 1.0000, utils::Color (255, 255, 255, 255)); // snow
 	renderer.EnableLight ();
 	renderer.SetLightContrast (3.0);
 	renderer.SetLightBrightness (2.0);
@@ -85,4 +199,4 @@ int main()
 	Writer.WriteDestFile();
 
 	cout << "Check the directory. It contains the generated map." << endl; 
-}
+}*/
