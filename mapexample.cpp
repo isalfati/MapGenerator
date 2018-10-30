a611dd78d6ba4f3558b308ded6ec4c5eb86ab29f
isalfati <ivansalfatift@gmail.com>
2018-10-31 00:24:25 +0100

    Playing

--- mapexample.cpp
+++ mapexample.cpp
@@ -35,14 +35,15 @@ int main()
 	cin >> contrast >> brightness;
 	
 	module::Perlin basicMap;
+	//module::RidgedMulti basicMap;
 	basicMap.SetFrequency(1.0); //Altough I think it is already defaulted at 1.
-	basicMap.SetLacunarity(2.0);
+	//basicMap.SetFrequency(1.5);
+	basicMap.SetLacunarity(2);
 	basicMap.SetNoiseQuality(QUALITY_BEST);
 	basicMap.SetOctaveCount(16);
-	//basicMap.SetPersistence(0.5);
-	basicMap.SetPersistence(0.60);
+	basicMap.SetPersistence(0.5);
 	basicMap.SetSeed(seed);
-
+	
 	utils::NoiseMap heightMap;
 	utils::NoiseMapBuilderPlane heightMapBuilder;
 	//heightMapBuilder.SetSourceModule(/* NAME FINAL TERRAIN */);
@@ -55,11 +56,26 @@ int main()
 	utils::NoiseMap mapNorm;
 	mapNorm.SetSize(width, height);
 	
+		double nh, maxnh, minnh;
+	maxnh = -10.0;
+	minnh =  10.0;
+	for(int i = 0; i < width; i++){
+		for(int j = 0; j < height; j++){
+			nh = heightMap.GetValue(i, j);
+			
+			if(nh > maxnh) maxnh = nh;
+			else if(nh < minnh) minnh = nh;
+			
+			heightMap.SetValue(i, j, nh);
+		}
+	}
 	
+	for(int i = 0; i < width; i++){
+		for(int j = 0; j < height; j++){
+			heightMap.SetValue(i, j, (heightMap.GetValue(i, j) - minnh ) / ( maxnh - minnh ));
+		}
+	}
 	
-	
-	
-	// Blood Moon Generator
 	double dx, dy, d, maxw, delta, gradient;
 	for(int x = 0; x < width; x++){
 		for(int y = 0; y < height; y++){
@@ -74,15 +90,10 @@ int main()
 			double value = max (0.0, 1.0 - gradient);
 			double vmap = heightMap.GetValue(x, y);
 			
-			//cout << "New Values: " << vmap*value << endl;
-			heightMap.SetValue(x, y, /*vmap - */value);
-			
-			if(x % width + 1  == 0){ cout << endl;
-				if(y % height + 1 == 0) cout << endl;
-			}
-			cout << value << " ";
-			
+			//cout << "value: " << value << ", vmap: " << vmap << " - ";
+			heightMap.SetValue(x, y, vmap * value);
 		}
+		//cout << endl;
 	}
 
 	utils::RendererImage renderer;
@@ -91,18 +102,20 @@ int main()
 	renderer.SetDestImage (image);
 	renderer.ClearGradient ();
 	
-	renderer.AddGradientPoint (-1.0000, utils::Color (  0,   0,   0, 255));
+	/*renderer.AddGradientPoint (-1.0000, utils::Color (255,   0,   0, 255));
 	renderer.AddGradientPoint ( 0.0000, utils::Color (255, 255, 255, 255));
-	renderer.AddGradientPoint ( 1.0000, utils::Color (  0,   0,   0, 255));
+	renderer.AddGradientPoint ( 1.0000, utils::Color (  0, 255,   0, 255));*/
 	
-	/*renderer.AddGradientPoint (-1.0000, utils::Color (  0,   0, 128, 255)); // deeps
-	renderer.AddGradientPoint (-0.2500, utils::Color (  0,   0, 255, 255)); // shallow
-	renderer.AddGradientPoint (-0.0625, utils::Color (  0, 128, 255, 255)); // shore
-	renderer.AddGradientPoint ( 0.0000, utils::Color (240, 240,  64, 255)); // sand
-	renderer.AddGradientPoint ( 0.1250, utils::Color ( 32, 160,   0, 255)); // grass
-	renderer.AddGradientPoint ( 0.3750, utils::Color (224, 224,   0, 255)); // dirt
-	renderer.AddGradientPoint ( 0.7500, utils::Color (128, 128, 128, 255)); // rock
-	renderer.AddGradientPoint ( 1.0000, utils::Color (255, 255, 255, 255)); // snow*/
+	renderer.AddGradientPoint ( 0.0000, utils::Color (  0,   0, 128, 255)); //deeps
+	renderer.AddGradientPoint ( 0.2000, utils::Color (  0,   0, 255, 255)); //shallow
+	renderer.AddGradientPoint ( 0.3000, utils::Color (  0, 128, 255, 255)); //shore
+	renderer.AddGradientPoint ( 0.3500, utils::Color (240, 240, 128, 255)); //sand
+	//renderer.AddGradientPoint ( 0.5500, utils::Color (224, 224,   0, 255)); //dirt
+	renderer.AddGradientPoint ( 0.5500, utils::Color (223, 191, 159, 255)); //dirt
+	renderer.AddGradientPoint ( 0.4500, utils::Color ( 32, 200,   0, 255)); //grass 
+	renderer.AddGradientPoint ( 0.7500, utils::Color (128, 128, 128, 255)); //rock
+	renderer.AddGradientPoint ( 0.9000, utils::Color (207,  16,  32, 255)); // Lava
+	renderer.AddGradientPoint ( 1.0000, utils::Color (255,   0,   0, 255)); //Pure RED
 	
 	renderer.EnableLight ();
 	renderer.SetLightContrast (contrast); //3.0
