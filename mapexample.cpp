526b7129b98bccd9c02c34ce72f4d8b9e016d83c
isalfati <ivansalfatift@gmail.com>
2018-11-17 17:11:57 +0100

    Playing

--- mapexample.cpp
+++ mapexample.cpp
@@ -43,8 +43,9 @@ int main()
 	cin >> contrast >> brightness;
 	
 	module::Perlin basicMap;
-	basicMap.SetFrequency(1.0); 
-	basicMap.SetLacunarity(2.2);
+//	basicMap.SetFrequency(1.0); 
+	basicMap.SetFrequency(1.25); 
+	basicMap.SetLacunarity(2.5);
 	basicMap.SetNoiseQuality(QUALITY_BEST);
 	basicMap.SetOctaveCount(16);
 	basicMap.SetPersistence(0.5);
@@ -58,8 +59,9 @@ int main()
 	heightMapBuilder.SetDestSize(width, height);
 	heightMapBuilder.SetBounds(xLow, xHigh, yLow, yHigh);
 	heightMapBuilder.Build();
-
+	
 	// Normalise
+	
 	double nh, maxnh, minnh;
 	maxnh = -10.0;
 	minnh =  10.0;
@@ -76,7 +78,7 @@ int main()
 	
 	for(int i = 0; i < width; i++){
 		for(int j = 0; j < height; j++){
-			heightMap.SetValue(i, j, (heightMap.GetValue(i, j) - minnh ) / ( maxnh - minnh ));
+			heightMap.SetValue(i, j, (heightMap.GetValue(i, j) - minnh ) / ( maxnh - minnh )+0.05);
 		}
 	}
 	
@@ -89,8 +91,7 @@ int main()
 			// Island
 			d = sqrt(dx*dx + dy*dy);
 			// More like a continent
-			d = max(dx, dy);
-			
+			//d = max(dx, dy);
 			
 			maxw = height * 0.5 - 10.0;
 			delta = d / maxw;
@@ -104,6 +105,48 @@ int main()
 		}
 		//cout << endl;
 	}//*/
+
+	/*utils::NoiseMap POI;
+	POI.SetSize(width, height);
+	for(int i = 0; i < width; i++)
+		for(int j = 0; j < height; j++)
+			POI.SetValue(i, j, 0.0000);
+			
+	int nPoi = rand() % height;
+	vector<Point> locationPoi (nPoi);
+	
+	for(int i = 0; i < nPoi; i++){
+		locationPoi[i].x = rand() % width;
+		locationPoi[i].y = rand() % height;
+	}
+	
+	for(int i = 0; i < locationPoi.size(); i++)
+		POI.SetValue(locationPoi[i].x, locationPoi[i].y, 1.0000);*/
+			
+
+
+
+	/*vector<double> rbiomes(8);
+	for(int i = 0; i < rbiomes.size(); i++) rbiomes[i] = (double) (rand() % 256 / (256.0));
+	int i1 = (rand() % rbiomes.size());
+	int i2 = (rand() % rbiomes.size());
+	double b1 = rbiomes[(rand() % rbiomes.size())]; 
+	double b2 = rbiomes[(rand() % 7)];
+	utils::NoiseMap biomes;
+	biomes.SetSize(width, height);
+	for(int i = 0; i < width; i++){
+		for(int j = 0; j < height; j++){
+			if((i>=j) and (i<(width/2))){
+				biomes.SetValue(i, j, b1);
+				//if(heightMap.GetValue(i, j) >= 0.5) heightMap.SetValue(i,j, -heightMap.GetValue(i,j));
+			}else{
+				biomes.SetValue(i,j, b2);
+			}
+//			biomes.SetValue(i, j, (double)i/width + (double)j/height);
+		}
+	}
+	// */
+	
 	
 	utils::RendererImage renderer;
 	utils::Image image;
@@ -111,36 +154,15 @@ int main()
 	renderer.SetDestImage (image);
 	renderer.ClearGradient ();
 	
-
-	/*renderer.AddGradientPoint (-1.0000, utils::Color (255,   0,   0, 255));
-	renderer.AddGradientPoint ( 0.0000, utils::Color (255, 255, 255, 255));
-	renderer.AddGradientPoint ( 1.0000, utils::Color (  0,   0,   0, 255));//*/
-	
-	//Render standard
-	/*renderer.AddGradientPoint (-1.0000, utils::Color (  0,   0, 128, 255)); // deeps
-	renderer.AddGradientPoint (-0.2500, utils::Color (  0,   0, 255, 255)); // shallow
-	renderer.AddGradientPoint (-0.0625, utils::Color (  0, 128, 255, 255)); // shore
-	renderer.AddGradientPoint ( 0.0000, utils::Color (240, 240,  64, 255)); // sand
-	renderer.AddGradientPoint ( 0.1250, utils::Color ( 32, 160,   0, 255)); // grass
-	renderer.AddGradientPoint ( 0.3750, utils::Color (224, 224,   0, 255)); // dirt
-	renderer.AddGradientPoint ( 0.7500, utils::Color (128, 128, 128, 255)); // rock
-	renderer.AddGradientPoint ( 1.0000, utils::Color (255, 255, 255, 255)); // snow */
-	
-	
 	//Render para mapas normalizados
 	renderer.AddGradientPoint (-1.0000, utils::Color (  0,   0,  64, 255));
-	renderer.AddGradientPoint (-0.5000, utils::Color (  0,   0, 128, 255)); // deeps
 	renderer.AddGradientPoint ( 0.1000, utils::Color (  0,   0, 255, 255)); // shallow
 	renderer.AddGradientPoint ( 0.3250, utils::Color (  0, 128, 255, 255)); // shore
 	renderer.AddGradientPoint ( 0.3500, utils::Color (240, 240, 128, 255)); // sand
 	renderer.AddGradientPoint ( 0.4000, utils::Color ( 32, 200,   0, 255)); // grass 
 	renderer.AddGradientPoint ( 0.6500, utils::Color (223, 191, 159, 255)); // dirt
-	//renderer.AddGradientPoint ( 0.7500, utils::Color (128, 128, 128, 255)); // rock
-	renderer.AddGradientPoint ( 0.7500, utils::Color (122, 102, 82, 255)); // rock
-	renderer.AddGradientPoint ( 0.9000, utils::Color (207,  16,  32, 255)); // Lava
-	renderer.AddGradientPoint ( 1.0000, utils::Color (255,   0,   0, 255)); // Pure RED
-	//renderer.AddGradientPoint ( 0.9000, utils::Color (128, 128, 128, 255));
-	//renderer.AddGradientPoint ( 1.0000, utils::Color (255, 255, 255, 255)); // */
+	renderer.AddGradientPoint ( 0.7500, utils::Color (122, 102,  82, 255)); // rock
+	renderer.AddGradientPoint ( 1.0000, utils::Color (255, 255, 255, 255)); // Snow */
 	
 	renderer.EnableLight ();
 	renderer.SetLightContrast (contrast); //3.0
@@ -152,6 +174,25 @@ int main()
 	
 	Writer.SetDestFilename(std::to_string(seed) + ".bmp");
 	Writer.WriteDestFile();
+	
+	/*utils::RendererImage r;
+	utils::Image i;
+	//r.SetSourceNoiseMap (biomes);
+	r.SetSourceNoiseMap(POI);
+	r.SetDestImage (i);
+	r.ClearGradient ();
+	r.AddGradientPoint(0.0000, utils::Color(0,0,0,255));
+	r.AddGradientPoint(1.0000, utils::Color(255,255,255,255));
+	r.EnableLight();
+	r.SetLightContrast(contrast);
+	r.SetLightBrightness(brightness);
+	r.Render();
+	utils::WriterBMP w;
+	w.SetSourceImage(i);
+	w.SetDestFilename(std::to_string(seed) + "_biome.bmp");
+	w.WriteDestFile();//*/
+	
+	
 	
 	cout << "Check the directory. It contains the generated map." << endl;
 }
